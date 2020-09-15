6451cf2c4f0d7d38b416097811ae24ea308943c5
masayado <masayado@uc.cl>
2020-09-15 18:55:01 -0300

    agregue funcion para imprimir 0 y edite estilos

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,102 @@
 <template>
+<center>
   <div id="app">
-    <div>{{ message }}</div>
-    <div>{{ countdown_msg }}</div>
-    <button @click="countdown3s">3s</button>
-    <button @click="countdown1m">1m</button>
-    <button @click="countdown5m">5m</button>
-    <button @click="countdown10m">10m</button>
-    <button @click="countdown30m">30m</button>
+    <div class="message">{{ message }}</div>
+    <span class="time">{{ time }}</span>
+    <div class="buttons">
+    <b-button variant="dark" @click="run(3)">3s</b-button>
+    <b-button variant="dark" @click="run(60)">1m</b-button>
+    <b-button variant="dark" @click="run(300)">5m</b-button>
+    <b-button variant="dark" @click="run(600)">10m</b-button>
+    <b-button variant="dark" @click="run(1800)">30m</b-button>
+    </div>
+    
   </div>
+</center>
 </template>
 
 <script>
+var interval;
   export default {
     data() {
       return {
         message: 'Cuenta regresiva',
-        countdown_msg: '00:00 min/seg'
+        time: "00:00",
       };
     },
     methods: {
+        run:function(seg){
+        const now = Date.now();
+        const end = now+(seg+1)*1000;
+        this.discount(end);
+        },
         countdown3s:function(){
-            alert("3s");
+            console.log("3s");
         },
         countdown1m:function(){
-            alert("1m");
+            console.log("1m");
         },
         countdown5m:function(){
-            alert("5m");
+            console.log("5m");
         },
         countdown10m:function(){
-            alert("10m");
+            console.log("10m");
         },
         countdown30m:function(){
-            alert("30m");
+            console.log("30m");
+        },
+        discount:function(end){
+            interval = setInterval(()=>{
+                const rest = Math.round((end-Date.now())/1000);
+                if (rest < 0){
+                    clearInterval(interval);
+                    return;
+                }
+                const minutes = Math.floor((rest % 3600)/60);
+                const seconds = rest % 60;
+                console.log(minutes, seconds);
+                //this.time = `${minutes}:${seconds}`;
+                if ((String(seconds).length)===1 && (String(minutes).length)===1 ){
+                    this.time=`0${minutes}:0${seconds}`
+                }else if((String(seconds).length)===1 && (String(minutes).length)!=1 ){
+                    this.time=`${minutes}:0${seconds}`
+                }else if((String(seconds).length)!=1 && (String(minutes).length)===1 ){
+                    this.time=`0${minutes}:${seconds}`
+                }else{
+                this.time=`${minutes}:${seconds}`
+                }
+            },1000);
         }
+
     }
 }
 </script>
 
 <style scoped>
   #app {
-    font-size: 25px;
-    font-family: "Calibri", sans-serif;
+    width:30%;
+    margin: 0 auto;
+    margin-top:3.75rem;
+    padding:1.25rem;
+    font-size: 1.5625rem;
+    font-family: $font-family-base;
     color: #000;
+    background-color:#fff;
+    border: 1px solid #000;
   }
+
+  .message{
+    font-weight: normal;
+  }
+
+  .buttons{
+    padding:10px;
+  }
+
+  .time{
+    font-family: 'Helvetica', sans-serif;
+    padding:10px;
+    font-weight:bold;
+  }
+
 </style>
